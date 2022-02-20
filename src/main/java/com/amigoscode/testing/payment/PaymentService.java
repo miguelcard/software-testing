c1765c30b2a10da2a362610b9cac19db2c8b4769
Cardenas Miguel <CARDENAM@brzg.local>
2022-02-20 21:57:28 +0100

    payment service addition

--- src/main/java/com/amigoscode/testing/payment/PaymentService.java
+++ src/main/java/com/amigoscode/testing/payment/PaymentService.java
@@ -23,30 +23,30 @@ public class PaymentService {
         this.paymentCharger = paymentCharger;
     }
 
-//    public void chargeCard(PaymentRequest request, UUID customerId) {
-//        // add logic and save payment
-//        //1. check customer exists, if not throw
-//        //2. do we support currency if not throw
-//        // 3. Charge card
-//        // 4. If not charged/debited throw
-//        // 5. Insert payment (db)
-//        // 6. TODO send sms
-//
-//        Optional<Customer> customerOptional = customerRepo.findById(customerId);
-//        if (!customerOptional.isPresent()) {
-//            throw new IllegalStateException(String.format("User with id [%s] not found!", customerId));
-//        }
-//        Payment payment = request.getPayment();
-//        if(!Arrays.stream(Currency.values()).anyMatch(currency -> currency.equals(payment.getCurrency()))){
-//            throw new IllegalStateException(String.format("Currency not supported [%s] ", payment.getCurrency().toString()));
-//        }
-//
-//        CardCharge charge = paymentCharger.chargeCard(payment.getAmount(), payment.getCurrency(), payment.getSource(), payment.getDescription());
-//        if(!charge.isWasCharged()) {
-//            throw new IllegalStateException("Card was not charged!");
-//        }
-//
-//        payment.setCustomerId(customerId);
-//        paymentRepo.save(payment);
-//    }
+    public void chargeCard(PaymentRequest request, UUID customerId) {
+        // add logic and save payment
+        //1. check customer exists, if not throw
+        //2. do we support currency if not throw
+        // 3. Charge card
+        // 4. If not charged/debited throw
+        // 5. Insert payment (db)
+        // 6. TODO send sms
+
+        Optional<Customer> customerOptional = customerRepo.findById(customerId);
+        if (!customerOptional.isPresent()) {
+            throw new IllegalStateException(String.format("User with id [%s] not found!", customerId));
+        }
+        Payment payment = request.getPayment();
+        if(!Arrays.stream(Currency.values()).anyMatch(currency -> currency.equals(payment.getCurrency()))){
+            throw new IllegalStateException(String.format("Currency not supported [%s] ", payment.getCurrency().toString()));
+        }
+
+        CardCharge charge = paymentCharger.chargeCard(payment.getAmount(), payment.getCurrency(), payment.getSource(), payment.getDescription());
+        if(!charge.isWasCharged()) {
+            throw new IllegalStateException("Card was not charged!");
+        }
+
+        payment.setCustomerId(customerId);
+        paymentRepo.save(payment);
+    }
 }
